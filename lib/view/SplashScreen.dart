0de3eca6f9155ed68348e7a92328744c0e1ce6f2
hyeminkim <orangetreex@gmail.com>
2024-01-21 10:20:01 +0900

    - 로그인 여부 체크로직 추가

--- lib/view/SplashScreen.dart
+++ lib/view/SplashScreen.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:papas/viewModel/UserViewModel.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(Duration(seconds: 3), () {
-      //context.go('/home');
+      checkUserLoginStatus();
     });
   }
 
@@ -29,5 +31,15 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       ),
     );
+  }
+
+  void checkUserLoginStatus() {
+    UserViewModel userStatus = Provider.of<UserViewModel>(context, listen: false);
+    print(userStatus.user);
+    if (userStatus.user != null) {
+      context.go('/home');
+    } else {
+      context.go('/login');
+    }
   }
 }
